ea11daf9fd729d7fb64473c7d7620fffe6024dc7
samantha-hong <96512457+samantha-hong@users.noreply.github.com>
2022-05-06 15:25:12 -0700

    array practice

---
+++ class22-space-review.js
@@ -0,0 +1,21 @@
+//Arrays
+
+//Create and array of numbers. Sum all of the numbers. Alert the sum.
+let nums = [8, 31, 2013, 4, 25];
+
+let sum = nums.reduce( (a,b) => a+b, 0);
+
+alert(sum);
+
+//Create a function that takes in an array of numbers
+//Return a new array of numbers that is every original number squared
+
+function squareNums(nums){
+    return nums.map(x => x**2)
+}
+
+//Create a function that takes string
+//Print the reverse of that string to the console
+
+//Create a function that takes in a string
+//Alert if the string is a palindrome or not
