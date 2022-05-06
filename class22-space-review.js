b2f4de6301749e441c5adcd5318c107c5db20c41
samantha-hong <96512457+samantha-hong@users.noreply.github.com>
2022-05-06 15:50:26 -0700

    array methods practice

--- class22-space-review.js
+++ class22-space-review.js
@@ -17,5 +17,19 @@ function squareNums(nums){
 //Create a function that takes string
 //Print the reverse of that string to the console
 
+function reverseString (str){
+    let rev = str.split('').reverse().join('');
+    console.log(rev)
+}
+
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+
+function palindrome(st){
+    let reversed = st.split('').reverse().join('');
+    if (st === reversed) {
+        alert('It is a palindrome!')
+    } else{
+        alert('It is not a palindrome!')
+    }
+}
